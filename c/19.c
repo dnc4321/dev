f3c2f86b1b90295db49f280b383b92ee0c5e4298
shatterball <brainhacker550@gmail.com>
2018-11-21 21:27:56 +0530

    Oh Yeah Progressing

--- c/19.c
+++ c/19.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void){
-    
+    int m, n;
+    printf("Enter number of rows: ");
+    scanf("%d", &m);
+    printf("Enter number of columns: ");
+    scanf("%d", &n);
+
+    int mat1[m][n], mat2[m][n];
+    printf("Enter matrix 1\n");
+    for(int i = 0; i < m; ++i){
+        printf("Enter row %d\n", i + 1);
+        for(int j = 0; j < n; ++j){
+            scanf("%d", &mat1[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Enter matrix 2\n");
+    for(int i = 0; i < m; ++i){
+        printf("Enter row %d\n", i + 1);
+        for(int j = 0; j < n; ++j){
+            scanf("%d", &mat2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Sum of the matrices:\n");
+    for(int i = 0; i < m; ++i){
+        for(int j = 0; j < n; ++j){
+            printf("%d ", mat2[i][j] + mat1[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
